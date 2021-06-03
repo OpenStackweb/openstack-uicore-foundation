240fc3ceeded3be3853a4b6baa3cfbca0c7991d4
smarcet <smarcet@gmail.com>
2021-06-03 15:44:19 -0300

    Update EditableTable
    added new attribute textArea
    to allow to set inputs as text areas
    
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/components/table-editable/EditableTableCell.js
+++ src/components/table-editable/EditableTableCell.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const EditableTableCell = (props) => {
+
     if (props.is_edit) {
         return (
             <td>
-                <input className="form-control" defaultValue={props.children} name={props.name} id={props.id} onChange={props.handleChange}/>
+                {!props.shouldUseTextArea &&
+                    <input type="text" className="form-control" defaultValue={props.children} name={props.name} id={props.id} onChange={props.handleChange}/>
+                }
+                {props.shouldUseTextArea &&
+                    <textarea className="form-control" defaultValue={props.children} name={props.name} id={props.id} onChange={props.handleChange}/>
+                }
             </td>
         );
     } else {
