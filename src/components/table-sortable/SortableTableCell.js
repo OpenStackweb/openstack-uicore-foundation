5efe7ca55aec78ae6eb668fbf6549030d706fd3e
Santi <santipalenque@gmail.com>
2021-07-28 13:06:00 -0300

    html in sortable table cell

--- src/components/table-sortable/SortableTableCell.js
+++ src/components/table-sortable/SortableTableCell.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import RawHTML from "../raw-html";
 
-const SortableTableCell = (props) => (
-	<td {...props}>{props.children}</td>
-);
+const SortableTableCell = (props) => {
+	let {children} = props;
+	let value = '';
+	if(children) {
+		if (React.isValidElement(children)) {
+			value = children;
+		} else {
+			value = <RawHTML>{children.toString()}</RawHTML>
+		}
+	}
+
+	return (
+		<td {...props}>
+			{value}
+		</td>
+	);
+};
 
 export default SortableTableCell;
