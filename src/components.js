463b060de2d56d9d8afc510f560ab0d6fd9631fb
smarcet <smarcet@gmail.com>
2021-12-13 18:23:31 -0300

    Added new Input Element
    TicketTypesInput
    
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/components.js
+++ src/components.js
@@ -49,6 +49,8 @@ import AttendanceTracker from "./components/attendance-tracker";
 import {getCurrentUserLanguage} from './utils/methods';
 import useFitText from './components/use-fit-text';
 import AccessLevelsInput from './components/inputs/access-levels-input';
+import TicketTypesInput from './components/inputs/ticket-types-input';
+
 let language = getCurrentUserLanguage();
 
 // language would be something like es-ES or es_ES
@@ -113,5 +115,6 @@ export {
     AttendanceTracker,
     SelectableTable,
     useFitText,
-    AccessLevelsInput
+    AccessLevelsInput,
+    TicketTypesInput,
 };
