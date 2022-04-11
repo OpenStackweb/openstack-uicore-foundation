189ead816cacab5e5b73e8ed6fd830b8b5fbe47b
Tomás Castillo <tcastilloboireau@gmail.com>
2022-04-11 19:26:12 -0300

    Updating react-dnd, refactoring sortable table
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/table-sortable/SortableTable.js
+++ src/components/table-sortable/SortableTable.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import update from 'immutability-helper';
-import { DragDropContext } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import SortableTableHeading from './SortableTableHeading';
 import SortableTableCell from './SortableTableCell';
 import SortableActionsTableCell from './SortableActionsTableCell';
 import SortableTableRow from './SortableTableRow';
 import T from 'i18n-react/dist/i18n-react';
 import './table-sortable.css';
-import {shallowEqual} from "../../utils/methods";
+import { shallowEqual } from "../../utils/methods";
 
 const defaults = {
     colWidth: ''
@@ -18,79 +18,60 @@ const defaults = {
 const createRow = (row, columns, actions) => {
 
     var action_buttons = '';
-	var cells = columns.map((col,i) => {
-		return (
-		<SortableTableCell key={i}>
+    var cells = columns.map((col, i) => {
+        return (
+        <SortableTableCell key={i}>
             {row[col.columnKey]}
-		</SortableTableCell>
-		);
-	});
+        </SortableTableCell>
+        );
+    });
 
     if (actions) {
-        cells.push(<SortableActionsTableCell key='actions' id={row['id']} actions={actions}/>);
+        cells.push(<SortableActionsTableCell key='actions' id={row['id']} actions={actions} />);
     }
 
     return cells;
 };
 
 
-class SortableTable extends React.Component {
+const SortableTable = ({ data,
+    options,
+    columns,
+    dropCallback,
+    orderField }) => {
 
-    constructor(props) {
-        super(props);
-        this.moveRow = this.moveRow.bind(this);
-        this.dropRow = this.dropRow.bind(this);
+    const [rows, setRows] = useState(data)
 
-        this.state = {
-            rows: props.data
-        };
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if(!shallowEqual(this.props.data, prevProps.data)) {
-            this.setState({rows: this.props.data})
-        }
-    }
-
-    moveRow(dragIndex, hoverIndex) {
-        const { rows } = this.state;
-        const dragRow = rows[dragIndex];
-
-        this.setState(update(this.state, {
-            rows: {
-                $splice: [
-                    [dragIndex, 1],
-                    [hoverIndex, 0, dragRow],
-                ],
-            },
-        }));
-    }
-
-    dropRow(id, index) {
-        let {orderField} = this.props;
-        let newOrder = index + 1;
-        let rows = [...this.state.rows];
-
-        for(var i in rows) {
-            rows[i][orderField] = parseInt(i) + 1;
-        }
-
-        this.setState(update(this.state, {rows: {$set: rows}}));
-
-        this.props.dropCallback(rows, id, newOrder);
-    }
-
-    render() {
-        let {options, columns} = this.props;
-        let tableClass = options.hasOwnProperty('className') ? options.className : '';
-
+    const renderRow = useCallback((row, columns, options, index) => {        
         return (
-            <div className="sortable-table-box">
-                <i>{T.translate("general.drag_and_drop")}</i>
-                <table className={"table table-striped table-hover sortableTable " + tableClass}>
-                    <thead>
+            <SortableTableRow even={index % 2 === 0} key={row.id} index={index} id={row.id} moveCard={moveRow} dropItem={onDropItem}>
+                {createRow(row, columns, options.actions)}
+            </SortableTableRow>
+        )
+    }, []);
+
+    const moveRow = useCallback((dragIndex, hoverIndex, id) => {
+        setRows((prevCards) => update(prevCards, {
+            $splice: [
+                [dragIndex, 1],
+                [hoverIndex, 0, prevCards[dragIndex]],
+            ],
+        }));
+    }, []);
+    
+    const onDropItem = (id, newOrder) => {
+        dropCallback(rows, id, newOrder)
+    }
+
+    let tableClass = options.hasOwnProperty('className') ? options.className : '';
+
+    return (
+        <div className="sortable-table-box">
+            <i>{T.translate("general.drag_and_drop")}</i>
+            <table className={"table table-striped table-hover sortableTable " + tableClass}>
+                <thead>
                     <tr>
-                        {columns.map((col,i) => {
+                        {columns.map((col, i) => {
                             let colWidth = (col.width) ? col.width : defaults.colWidth;
                             return (
                                 <SortableTableHeading width={colWidth} key={i} >
@@ -104,24 +85,23 @@ class SortableTable extends React.Component {
                         </SortableTableHeading>
                         }
                     </tr>
-                    </thead>
-                    <tbody>
-                    {columns.length > 0 && this.state.rows.map((row,i) => {
-                        if(Array.isArray(row) && row.length !== columns.length) {
+                </thead>
+                <tbody>
+                    {columns.length > 0 && rows.map((row, i) => {
+                        if (Array.isArray(row) && row.length !== columns.length) {
                             console.warn(`Data at row ${i} is ${row.length}. It should be ${columns.length}.`);
                             return <tr />
                         }
                         return (
-                            <SortableTableRow even={i%2 === 0} key={row.id} index={i} id={row.id} moveCard={this.moveRow} dropCard={this.dropRow}>
-                                {createRow(row, columns, options.actions)}
-                            </SortableTableRow>
+                            <DndProvider backend={HTML5Backend}>
+                                {renderRow(row, columns, options, i)}
+                            </DndProvider>
                         );
                     })}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 SortableTable.propTypes = {
@@ -138,4 +118,4 @@ SortableTable.propTypes = {
     dropCallback: PropTypes.func.isRequired
 }
 
-export default DragDropContext(HTML5Backend)(SortableTable);
+export default SortableTable;
