81e7a13c89d3e2000bc4fa20aae3b54180cb489f
Tomás Castillo <tomrndom@users.noreply.github.com>
2022-04-19 12:01:04 -0300

    Updating react-dnd, refactoring sortable table (#46)
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/table-sortable/SortableTableRow.js
+++ src/components/table-sortable/SortableTableRow.js
@@ -1,107 +1,92 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 import PropTypes from 'prop-types';
-import { findDOMNode } from 'react-dom';
-import { DragSource, DropTarget } from 'react-dnd';
 
-const style = {
-    border: '1px dashed gray',
-    padding: '0.5rem 1rem',
-    marginBottom: '.5rem',
-    backgroundColor: 'white',
-    cursor: 'move',
-};
+const SortableTableRow = ({ text, even, id, index, moveCard, dropItem, children }) => {
 
-const rowSource = {
-    beginDrag(props) {
-        return {
-            id: props.id,
-            index: props.index,
-        };
-    },
-    endDrag(props, monitor) {
-        if (monitor.didDrop()) {
-            props.dropCard(props.id, props.index);
-        }
-    }
-};
+    const style = {
+        border: '1px dashed gray',
+        padding: '0.5rem 1rem',
+        marginBottom: '.5rem',
+        backgroundColor: 'white',
+        cursor: 'move',
+    };
 
-const rowTarget = {
-    hover(props, monitor, component) {
-        const dragIndex = monitor.getItem().index;
-        const hoverIndex = props.index;
+    const refRow = useRef(null);
+    const [{ handlerId }, drop] = useDrop({
+        accept: 'row',
+        collect(monitor) {
+            return {
+                handlerId: monitor.getHandlerId(),
+            };
+        },
+        drop(_item, monitor) {
+            return dropItem(id, index + 1)
+        },
+        hover(item, monitor) {
+            if (!refRow.current) {
+                return;
+            }
+            const dragIndex = item.index;
+            const hoverIndex = index;
+            // Don't replace items with themselves
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+            // Determine rectangle on screen
+            const hoverBoundingRect = refRow.current?.getBoundingClientRect();
+            // Get vertical middle
+            const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+            // Determine mouse position
+            const clientOffset = monitor.getClientOffset();
+            // Get pixels to the top
+            const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+            // Only perform the move when the mouse has crossed half of the items height
+            // When dragging downwards, only move when the cursor is below 50%
+            // When dragging upwards, only move when the cursor is above 50%
+            // Dragging downwards
+            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+                return;
+            }
+            // Dragging upwards
+            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+                return;
+            }
+            // Time to actually perform the action
+            moveCard(dragIndex, hoverIndex);
+            // Note: we're mutating the monitor item here!
+            // Generally it's better to avoid mutations,
+            // but it's good here for the sake of performance
+            // to avoid expensive index searches.
+            item.index = hoverIndex;
+        },
+    });
+    const [{ isDragging }, drag] = useDrag({
+        type: 'row',
+        item: () => {
+            return { id, index };
+        },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
 
-        // Don't replace items with themselves
-        if (dragIndex === hoverIndex) {
-            return;
-        }
+    const opacity = isDragging ? 0 : 1;
 
-        // Determine rectangle on screen
-        const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+    drag(drop(refRow));
 
-        // Get vertical middle
-        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-        // Determine mouse position
-        const clientOffset = monitor.getClientOffset();
-
-        // Get pixels to the top
-        const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-        // Only perform the move when the mouse has crossed half of the items height
-        // When dragging downwards, only move when the cursor is below 50%
-        // When dragging upwards, only move when the cursor is above 50%
-
-        // Dragging downwards
-        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-            return;
-        }
-
-        // Dragging upwards
-        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-            return;
-        }
-
-        // Time to actually perform the action
-        props.moveCard(dragIndex, hoverIndex);
-
-        // Note: we're mutating the monitor item here!
-        // Generally it's better to avoid mutations,
-        // but it's good here for the sake of performance
-        // to avoid expensive index searches.
-        monitor.getItem().index = hoverIndex;
-    },
-};
-
-class SortableTableRow extends React.Component {
-
-    render() {
-        const { text, isDragging, connectDragSource, connectDropTarget, children, even, id } = this.props;
-        const opacity = isDragging ? 0 : 1;
-
-        return connectDragSource(connectDropTarget(
-            <tr id={id} role="row" className={even ? 'even' : 'odd'} style={{ ...style, opacity }}>
-		        {children}
-            </tr>,
-        ));
-    }
+    return (
+        <tr id={id} ref={refRow} role="row" className={even ? 'even' : 'odd'} style={{ ...style, opacity }}>
+            {children}
+        </tr>
+    );
 }
 
 SortableTableRow.propTypes = {
-    connectDragSource: PropTypes.func.isRequired,
-    connectDropTarget: PropTypes.func.isRequired,
     index: PropTypes.number.isRequired,
-    isDragging: PropTypes.bool.isRequired,
     id: PropTypes.any.isRequired,
     moveCard: PropTypes.func.isRequired,
 };
 
-export default
-    DragSource('card', rowSource, (connect, monitor) => ({
-        connectDragSource: connect.dragSource(),
-        isDragging: monitor.isDragging(),
-        }))(
-    DropTarget('card', rowTarget, connect => ({
-        connectDropTarget: connect.dropTarget(),
-        }))
-    (SortableTableRow));
+export default SortableTableRow;
 
