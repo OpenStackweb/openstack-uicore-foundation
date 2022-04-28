0fda40aa2613a5ce3a9f52e0e2970ceaf0c25c87
Tomás Castillo <tcastilloboireau@gmail.com>
2022-04-28 10:19:24 -0300

    Adding onEror as required on proptypes
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/inputs/registration-company-input.js
+++ src/components/inputs/registration-company-input.js
@@ -12,6 +12,7 @@
  **/
 
 import React from 'react';
+import PropTypes from 'prop-types'
 import AsyncSelect from 'react-select/lib/Async';
 import { queryRegistrationCompanies } from '../../utils/query-actions';
 
@@ -70,7 +71,7 @@ export default class RegistrationCompanyInput extends React.Component {
         // we need to map into value/label because of a bug in react-select 2
         // https://github.com/JedWatson/react-select/issues/2998
 
-        const translateOptions = (options) => {            
+        const translateOptions = (options) => {
             if (options instanceof Error) {
                 this.props.onError(options);
             }
@@ -138,3 +139,7 @@ export default class RegistrationCompanyInput extends React.Component {
 
     }
 }
+
+RegistrationCompanyInput.propTypes = {
+    onError: PropTypes.func.isRequired
+};
