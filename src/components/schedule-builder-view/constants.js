faa0cafd5debc819277b3190099d6f68d970acb0
Tomás Castillo <tcastilloboireau@gmail.com>
2023-03-20 11:34:46 -0300

    Revert custom slot option prop, add new constant value
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/schedule-builder-view/constants.js
+++ src/components/schedule-builder-view/constants.js
@@ -4,7 +4,7 @@ export const DraggableItemTypes = {
 };
 
 export const TBALocation = {id : 0, name : 'TBD', class_name: 'SummitVenue'};
-export const SlotSizeOptions = [5,10,15,30,60]; // 12 - 6 - 4 - 2 - 1
+export const SlotSizeOptions = [1,5,10,15,30,60]; // 12 - 6 - 4 - 2 - 1
 export const PixelsPerMinute = 3;
 export const bulkOptions = [
     {value: 'BULK_ACTION_EDIT', label: 'Edit'},
