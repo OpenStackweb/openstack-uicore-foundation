446a36d7c5a1180b3fcdeea713b76a7810767484
Santiago Palenque <santipalenque@gmail.com>
2023-04-06 11:37:55 -0300

    add disabled flags to media upload input v2

--- src/components/inputs/upload-input-v2/index.js
+++ src/components/inputs/upload-input-v2/index.js
@@ -23,14 +23,18 @@ export default class UploadInputV2 extends React.Component {
     constructor(props) {
         super(props);
     }
-
-    render() {
-        let {value, onRemove, error, mediaType, postUrl, maxFiles = 1, timeOut, onUploadComplete, djsConfig, id } = this.props;
-        let has_error = ( this.props.hasOwnProperty('error') && error !== '' );
+    
+    getDropzone = () => {
+        const {value, onRemove, canAdd = true, mediaType, postUrl, maxFiles = 1, timeOut, onUploadComplete, djsConfig, id } = this.props;
         const allowedExt = mediaType && mediaType.type ? mediaType.type.allowed_extensions.map((ext) => `.${ext.toLowerCase()}`).join(",") : '';
         const maxSize = mediaType ? mediaType.max_size / 1024 : 100;
         const canUpload = !maxFiles || value.length < maxFiles;
-
+        
+        let eventHandlers = {};
+        if (onRemove) {
+            eventHandlers = {removedfile: onRemove};
+        }
+    
         const djsConfigSet = {
             paramName: "file", // The name that will be used to transfer the file,
             maxFilesize: maxSize, // MB,
@@ -44,6 +48,7 @@ export default class UploadInputV2 extends React.Component {
             dropzoneSelector: `media_upload_${mediaType.id}`,
             ...djsConfig
         };
+        
         const componentConfig = {
             showFiletypeIcon: false,
             postUrl: postUrl
@@ -52,31 +57,42 @@ export default class UploadInputV2 extends React.Component {
             media_type: mediaType,
             media_upload: value,
         };
-
-        let eventHandlers = {};
-        if (onRemove) {
-            eventHandlers = {removedfile: onRemove};
+        
+        if (!canAdd) {
+            return (
+                <div className="filepicker disabled">
+                    Upload has been disabled by administrators.
+                </div>
+            );
+        } else if (!canUpload) {
+            return (
+                <div className="filepicker disabled">
+                    Max number of files uploaded for this type - Remove uploaded file to add new file.
+                </div>
+            );
+        } else {
+            return (
+                <DropzoneJS
+                    id={id}
+                    djsConfig={djsConfigSet}
+                    config={componentConfig}
+                    eventHandlers={eventHandlers}
+                    data={data}
+                    uploadCount={value.length}
+                    onUploadComplete={onUploadComplete}
+                />
+            );
         }
+    }
+
+    render() {
+        const {value, canDelete = true, onRemove, error} = this.props;
+        const has_error = ( this.props.hasOwnProperty('error') && error !== '' );
 
         return (
             <div className="row">
                 <div className="col-md-6"  style={{height: 180}}>
-                    {canUpload ? (
-                        <DropzoneJS
-                            id={id}
-                            djsConfig={djsConfigSet}
-                            config={componentConfig}
-                            eventHandlers={eventHandlers}
-                            data={data}
-                            uploadCount={value.length}
-                            onUploadComplete={onUploadComplete}
-                        />
-                    ) : (
-                        <div className="filepicker disabled">
-                            Max number of files uploaded for this type - Remove uploaded file to add new file.
-                        </div>
-                    )}
-
+                    {this.getDropzone()}
                 </div>
                 <div className="col-md-6">
                     {has_error &&
@@ -108,7 +124,7 @@ export default class UploadInputV2 extends React.Component {
                                             </a>
                                         </span>
                                         <span className="file-name"><a href={src} target="_blank" title="See Preview">{`${path}.${ext}`}</a></span>
-                                        {onRemove &&
+                                        {onRemove && canDelete &&
                                             <span className="file-delete">
                                                 <a href="#" data-tip="delete" title="Delete File" onClick={ev => onRemove(v)} >
                                                     <i className="fa fa-trash-o delete-icon"></i>
