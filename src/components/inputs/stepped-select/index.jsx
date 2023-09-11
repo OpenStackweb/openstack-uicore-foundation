0435e47fbdc932e9d97636f505cc3d733a8750d6
Tomás Castillo <tcastilloboireau@gmail.com>
2023-09-11 19:53:44 -0300

    Change behaviour for step buttons, disable buttons on last step
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/inputs/stepped-select/index.jsx
+++ src/components/inputs/stepped-select/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from './index.module.less';
 
 export default ({value, options, onChange, ...rest}) => {
+  
+  const currentOptionKey = options.findIndex(op => op.value === value);
 
-  const handleChange = (increment) => {
-    const currentOptionKey = options.findIndex(op => op.value === value);
+  const handleChange = (decress) => {
     if (currentOptionKey >= 0) {
-      if (increment) {
+      if (decress) {
         if (currentOptionKey < options.length -1) {
           onChange(options[currentOptionKey + 1].value);
         }
@@ -22,11 +23,11 @@ export default ({value, options, onChange, ...rest}) => {
 
   return (
     <div className={styles.wrapper} {...rest}>
-      <button className="btn btn-default" onClick={() => handleChange(false)}>
+      <button className="btn btn-default" onClick={() => handleChange(true)} disabled={currentOptionKey + 1 === options.length}>
         <i className="fa fa-minus"/>
       </button>
       <span className={styles.valueBox}>{valueLabel}</span>
-      <button className="btn btn-default" onClick={() => handleChange(true)}>
+      <button className="btn btn-default" onClick={() => handleChange(false)} disabled={currentOptionKey === 0}>
         <i className="fa fa-plus"/>
       </button>
     </div>
