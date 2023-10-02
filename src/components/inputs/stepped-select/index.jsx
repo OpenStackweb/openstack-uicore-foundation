07e1486e3c4ad59d4a90867c57abae6e5b86ede6
Tomás Castillo <tomrndom@users.noreply.github.com>
2023-10-02 16:58:06 -0300

    Change behaviour for step buttons, disable buttons on last step (#125)
    * Change behaviour for step buttons, disable buttons on last step
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Refactoring
    
    ---------
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    Co-authored-by: smarcet <smarcet@gmail.com>

--- src/components/inputs/stepped-select/index.jsx
+++ src/components/inputs/stepped-select/index.jsx
@@ -2,31 +2,30 @@ import React from 'react';
 import styles from './index.module.less';
 
 export default ({value, options, onChange, ...rest}) => {
-
-  const handleChange = (increment) => {
-    const currentOptionKey = options.findIndex(op => op.value === value);
-    if (currentOptionKey >= 0) {
-      if (increment) {
-        if (currentOptionKey < options.length -1) {
-          onChange(options[currentOptionKey + 1].value);
-        }
-      } else {
-        if (currentOptionKey > 0) {
-          onChange(options[currentOptionKey - 1].value);
-        }
-      }
-    }
-  };
+  
+  const currentOptionKey = options.findIndex(op => op.value === value);
 
   const valueLabel = options.find(op => op.value === value).label;
 
+  const onClickMinus = () => {
+    if (currentOptionKey > 0) {
+        onChange(options[currentOptionKey - 1].value);
+    }
+  }
+
+  const onClickPlus = () => {
+      if (currentOptionKey < options.length -1) {
+        onChange(options[currentOptionKey + 1].value);
+    }
+  }
+
   return (
     <div className={styles.wrapper} {...rest}>
-      <button className="btn btn-default" onClick={() => handleChange(false)}>
+      <button className="btn btn-default" onClick={() => onClickMinus()} disabled={currentOptionKey === 0} title="Decrement">
         <i className="fa fa-minus"/>
       </button>
       <span className={styles.valueBox}>{valueLabel}</span>
-      <button className="btn btn-default" onClick={() => handleChange(true)}>
+      <button className="btn btn-default" onClick={() => onClickPlus()} disabled={currentOptionKey + 1 === options.length} title="Increment">
         <i className="fa fa-plus"/>
       </button>
     </div>
