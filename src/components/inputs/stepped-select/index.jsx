a80b27471ff66bdb87a82896d4d194f842bd063a
smarcet <smarcet@gmail.com>
2023-10-02 16:57:02 -0300

    Refactoring

--- src/components/inputs/stepped-select/index.jsx
+++ src/components/inputs/stepped-select/index.jsx
@@ -5,29 +5,27 @@ export default ({value, options, onChange, ...rest}) => {
   
   const currentOptionKey = options.findIndex(op => op.value === value);
 
-  const handleChange = (decress) => {
-    if (currentOptionKey >= 0) {
-      if (decress) {
-        if (currentOptionKey < options.length -1) {
-          onChange(options[currentOptionKey + 1].value);
-        }
-      } else {
-        if (currentOptionKey > 0) {
-          onChange(options[currentOptionKey - 1].value);
-        }
-      }
-    }
-  };
-
   const valueLabel = options.find(op => op.value === value).label;
+
+  const onClickMinus = () => {
+    if (currentOptionKey > 0) {
+        onChange(options[currentOptionKey - 1].value);
+    }
+  }
+
+  const onClickPlus = () => {
+      if (currentOptionKey < options.length -1) {
+        onChange(options[currentOptionKey + 1].value);
+    }
+  }
 
   return (
     <div className={styles.wrapper} {...rest}>
-      <button className="btn btn-default" onClick={() => handleChange(true)} disabled={currentOptionKey + 1 === options.length}>
+      <button className="btn btn-default" onClick={() => onClickMinus()} disabled={currentOptionKey === 0} title="Decrement">
         <i className="fa fa-minus"/>
       </button>
       <span className={styles.valueBox}>{valueLabel}</span>
-      <button className="btn btn-default" onClick={() => handleChange(false)} disabled={currentOptionKey === 0}>
+      <button className="btn btn-default" onClick={() => onClickPlus()} disabled={currentOptionKey + 1 === options.length} title="Increment">
         <i className="fa fa-plus"/>
       </button>
     </div>
