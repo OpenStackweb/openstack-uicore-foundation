ec8f22a6ff85548b780d61323e2fabbc7eee3613
Tomás Castillo <tcastilloboireau@gmail.com>
2023-12-01 10:40:03 -0300

    Adjusting checkbox question style to align properly checkbox and label
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/extra-questions/index.js
+++ src/components/extra-questions/index.js
@@ -307,9 +307,8 @@ const ExtraQuestionsForm = React.forwardRef(({
         if (q.type === QuestionType_Checkbox) {
             return (
                 <Fragment key={toSlug(q.name)}>
-                    <div ref={el => questionRef.current[q.id] = el} style={{display: 'flex'}}
-                         className={questionContainerClassName}>
-                        <div className={questionControlContainerClassName}>
+                    <div ref={el => questionRef.current[q.id] = el} className={`${questionContainerClassName} checkbox-wrapper`}>
+                        <div className={`${questionControlContainerClassName} input-wrapper`}>
                             <div className="form-check abc-checkbox">
                                 <Field
                                        name={toSlug(q.name)}
@@ -323,7 +322,7 @@ const ExtraQuestionsForm = React.forwardRef(({
                                 <label className="form-check-label" htmlFor={toSlug(q.name)}/>
                             </div>
                         </div>
-                        <RawHTML replaceNewLine={true} className={`eq-checkbox-label ${questionLabelContainerClassName}`}>
+                        <RawHTML replaceNewLine={true} className={`eq-checkbox-label ${questionLabelContainerClassName}`} style={{paddingTop: 0}}>
                             {getLabel(q)}
                         </RawHTML>
                     </div>
--- src/components/raw-html/index.js
+++ src/components/raw-html/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const RawHTML = ({children, replaceNewLine = false, className = ""}) =>
+const RawHTML = ({children, replaceNewLine = false, className = "", ...rest}) =>
     <span className={className}
-          dangerouslySetInnerHTML={{ __html: replaceNewLine ? children?.replace(/\n/g, '<br />') : children}} />
+          dangerouslySetInnerHTML={{ __html: replaceNewLine ? children?.replace(/\n/g, '<br />') : children}} {...rest}/>
 
 export default RawHTML;
