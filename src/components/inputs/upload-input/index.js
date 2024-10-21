61f763950350af51305848087fc8934a216fd655
Santiago Palenque <santipalenque@gmail.com>
2024-10-21 12:28:53 -0300

    refactor uploadInput

--- src/components/inputs/upload-input/index.js
+++ src/components/inputs/upload-input/index.js
@@ -11,7 +11,7 @@
  * limitations under the License.
  **/
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Dropzone from 'react-dropzone';
 import T from 'i18n-react/dist/i18n-react';
 import './upload.less';
@@ -22,116 +22,102 @@ import mov_icon from './mov.png';
 import mp4_icon from './mp4.png';
 import csv_icon from './csv.png';
 
-export default class UploadInput extends React.Component {
+const fileHasPreview = (fileName) => {
+    if (!fileName) return false;
+    const pattern = /(.*)\.(gif|bmp|svg|jpe?g|png)/g
+    return pattern.test(fileName);
+}
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            show_remove: false,
-            logo_preview: {
-                preview: null,
-                name: '',
-            },
-        }
-
-        this.fileHasPreview = this.fileHasPreview.bind(this);
+const getPreviewIcon = (value, fileName) => {
+    if (value && fileHasPreview(fileName)) {
+        return value;
     }
 
-    onDrop(acceptedFiles, fileRejections, evt) {        
-        if(acceptedFiles.length > 0) {
-            let file = acceptedFiles[0];
-            this.props.handleUpload(file, this.props);
-            this.setState({
-                    ...this.state,
-                    logo_preview:
-                    {
-                        preview: this.fileHasPreview(file.name) ? file.preview: null,
-                        name: file.name
-                    }
-                })
-        }
-        if(fileRejections.length > 0 && this.props.hasOwnProperty("handleError"))
-            this.props.handleError(fileRejections, this.props)
-    }
+    const ext = fileName.split('.').pop();
 
-    onRemove(ev){
-        this.props.handleRemove(ev, this.props);
-    }
-
-    showVeil() {
-        this.setState({...this.state, show_remove:true});
-    }
-
-    hideVeil() {
-        this.setState({...this.state, show_remove:false});
-    }
-
-    fileHasPreview(fileName){
-        if(!fileName) return false;
-        let pattern = /(.*)\.(gif|bmp|svg|jpe?g|png)/g
-        return pattern.test(fileName);
-    }
-
-    render() {
-        let {value, file, handleRemove, handleUpload, handleError, fileName, error, ...rest} = this.props;
-        let {logo_preview, show_remove} = this.state;
-        let has_error = ( this.props.hasOwnProperty('error') && error !== '' );
-        // default value ( resource )
-        let icon = file_icon;
-
-        if (value) {
-            if(!fileName) fileName = value.slice(value.lastIndexOf("/") + 1);
-        }
-
-        if (file && file.hasOwnProperty("name") && file.name) {
-            fileName = file.name;
-        }
-
-         // files without preview
-        icon =  (fileName && fileName.endsWith('pdf') ? pdf_icon: icon);
-        icon =  (fileName && fileName.endsWith('mov') ? mov_icon: icon);
-        icon =  (fileName && fileName.endsWith('mp4') ? mp4_icon: icon);
-        icon =  (fileName && fileName.endsWith('csv') ? csv_icon: icon);
-
-        // if file has preview ( already on data model)
-        if(this.fileHasPreview(fileName) && value){
-            icon = value
-        }
-
-        if (logo_preview.preview && logo_preview.name) {
-            icon = logo_preview.preview;
-            fileName = logo_preview.name;
-        }
-
-        return (
-            <div className="file-upload">
-                <Dropzone
-                    onDrop={this.onDrop.bind(this)}
-                    {...rest}
-                >
-                <div>{T.translate("general.drop_files")}</div>
-                </Dropzone>
-                <div className="selected-files-box col-md-6">
-                    <p>Selected Files</p>
-                    <div className="selected-files">
-                        {value &&
-                        <div className="file-box" onMouseEnter={this.showVeil.bind(this)} onMouseLeave={this.hideVeil.bind(this)}>
-                            <img src={icon} />
-                            <a href={value} target="_blank">{fileName}</a>
-                            {show_remove &&
-                            <div className="remove" onClick={this.onRemove.bind(this)}>
-                                <i className="fa fa-times"></i>
-                            </div>
-                            }
-                        </div>
-                        }
-                        {has_error &&
-                        <p className="error-label">{error}</p>
-                        }
-                    </div>
-                </div>
-            </div>
-        )
+    switch (ext) {
+        case 'pdf':
+            return pdf_icon;
+        case 'mov':
+            return mov_icon;
+        case 'mp4':
+            return mp4_icon;
+        case 'csv':
+            return csv_icon;
+        default:
+            return file_icon;
     }
 }
+
+const UploadInput = ({value, error, handleRemove, handleUpload, handleError, ...rest}) => {
+    const [showRemove, setShowRemove] = useState(false);
+    const [logoPreview, setLogoPreview] = useState({preview: null, name: ''});
+
+    useEffect(() => {
+        const logoPreviewTmp = {preview: null, name: ''};
+
+        if (value) {
+            const fileName = value.split("/").pop();
+            logoPreviewTmp.preview = getPreviewIcon(value, fileName);
+            logoPreviewTmp.name = fileName;
+            setLogoPreview(logoPreviewTmp);
+        }
+    }, [value]);
+
+    const onDrop = (acceptedFiles, fileRejections) => {
+        if (acceptedFiles.length > 0) {
+            const file = acceptedFiles[0];
+            handleUpload(file);
+        }
+
+        if (fileRejections.length > 0 && handleError)
+            handleError(fileRejections);
+    }
+
+    const onRemove = (ev) => {
+        ev.preventDefault();
+        handleRemove();
+    }
+
+    const showVeil = () => {
+        setShowRemove(true)
+    }
+
+    const hideVeil = () => {
+        setShowRemove(false);
+    }
+
+    const has_error = error !== '';
+
+    return (
+        <div className="file-upload">
+            <Dropzone
+                onDrop={onDrop}
+                {...rest}
+            >
+                <div>{T.translate("general.drop_files")}</div>
+            </Dropzone>
+            <div className="selected-files-box col-md-6">
+                <p>Selected Files</p>
+                <div className="selected-files">
+                    {value &&
+                        <div className="file-box" onMouseEnter={showVeil} onMouseLeave={hideVeil}>
+                            <img src={logoPreview.preview} />
+                            <a href={value} target="_blank">{logoPreview?.name}</a>
+                            {showRemove &&
+                                <div className="remove" onClick={onRemove}>
+                                    <i className="fa fa-times"></i>
+                                </div>
+                            }
+                        </div>
+                    }
+                    {has_error &&
+                        <p className="error-label">{error}</p>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UploadInput;
