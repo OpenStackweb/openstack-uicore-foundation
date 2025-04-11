317ebbe4f889e54d5ddd0e812079c9c6dc98d437
Santiago Palenque <santipalenque@gmail.com>
2025-04-11 15:05:26 -0300

    update rte v3

--- src/components/inputs/editor-input-v3.js
+++ src/components/inputs/editor-input-v3.js
@@ -1,7 +1,14 @@
 import React, { useRef, useMemo } from "react";
 import JoditEditor from "jodit-react";
 
-const TextEditorV3 = ({ id, value, error, className, onChange, options = {} }) => {
+const TextEditorV3 = ({
+                          id,
+                          value,
+                          error,
+                          className,
+                          onChange,
+                          options = {}
+                      }) => {
     const editor = useRef(null);
 
     // all options from https://xdsoft.net/jodit/docs/,
@@ -9,21 +16,49 @@ const TextEditorV3 = ({ id, value, error, className, onChange, options = {} }) =
         () => ({
             placeholder: "Start typings...",
             className,
-            buttons: ["bold", "italic", "strikethrough", "underline", "|", "source", "|", "font", "fontsize","align", "|", "ul", "ol", "image", "link", "|", "undo", "redo"],
+            buttons: [
+                "bold",
+                "italic",
+                "strikethrough",
+                "underline",
+                "|",
+                "source",
+                "|",
+                "font",
+                "fontsize",
+                "align",
+                "|",
+                "ul",
+                "ol",
+                "image",
+                "link",
+                "|",
+                "undo",
+                "redo"
+            ],
+            askBeforePasteHTML: false,
+            askBeforePasteFromWord: false,
+            pasteFromWord: {
+                enable: true,
+                convertUnitsToPixel: true
+            },
+            beautifyHTML: false,
             ...options
         }),
         [options, className]
     );
 
     const handleChange = (newValue) => {
-        const ev = {target: {
+        const ev = {
+            target: {
                 id,
                 value: newValue,
                 type: "texteditor"
-            }};
+            }
+        };
 
         onChange(ev);
-    }
+    };
 
     return (
         <div className="editor-input">
@@ -33,9 +68,7 @@ const TextEditorV3 = ({ id, value, error, className, onChange, options = {} }) =
                 config={config}
                 onBlur={handleChange} // preferred to use only this option to update the content for performance reasons
             />
-            {error &&
-                <p className="error-label">{error}</p>
-            }
+            {error && <p className="error-label">{error}</p>}
         </div>
     );
 };
