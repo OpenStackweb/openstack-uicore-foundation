ec7932d445fc873f1ab05b0c1c76eab6bb1fb188
Santiago Palenque <santipalenque@gmail.com>
2026-05-18 11:43:05 -0300

    chore: move GridFilter to uicore

---
+++ src/components/mui/GridFilter/hooks/useGridFilter.jsx
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import { saveFilters } from "../actions/filter-actions";
+import { JOIN_OPERATORS } from "../utils";
+
+const useGridFilter = (id) => {
+  const dispatch = useDispatch();
+  const allFilters = useSelector(
+    (state) => state.allGridFiltersState.allFilters
+  );
+  const filter = allFilters.find((f) => f.id === id) || {};
+  const {
+    filterValues = [],
+    joinOperator = JOIN_OPERATORS.ALL,
+    parsedFilter = []
+  } = filter;
+
+  const valuesWithIds = filterValues.map((v, i) => ({
+    ...v,
+    id: `${v.criteria}-${i}`
+  }));
+
+  const resetFilters = () => dispatch(saveFilters(id));
+
+  return {
+    filterValues,
+    filterCount: filterValues.length,
+    joinOperator,
+    parsedFilter,
+    valuesWithIds,
+    resetFilters
+  };
+};
+
+export default useGridFilter;
