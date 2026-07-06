9e9d4793f67ba9014dcb2d659f2a38ef352a809f
Tomás Castillo <tomrndom@users.noreply.github.com>
2026-07-06 11:23:22 -0300

    feat: add mui formik text editor (#277)
    * feat: add mui formik text editor
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * fix: label prop and conditional rendering
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * fix: export component from webpack
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    ---------
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/components/mui/formik-inputs/mui-formik-text-editor.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useField } from "formik";
+import { InputLabel } from "@mui/material";
+import TextEditorV3 from "../../inputs/editor-input-v3";
+import { normalizeHtmlString } from "../../../utils/methods";
+
+const FormikTextEditor = ({ name, label = "", options = {}, licence, ...props }) => {
+  const [field, meta, helpers] = useField(name);
+  const mergedOptions = { tabIndex: 0, allowTabNavigation: true, ...options };
+
+  return (
+    <>
+      {label && (
+        <InputLabel htmlFor={name}>
+          {label}
+        </InputLabel>
+      )}
+      <TextEditorV3
+        id={name}
+        value={field.value}
+        options={mergedOptions}
+        onChange={(e) => {
+          const stringValue = normalizeHtmlString(e.target.value);
+          helpers.setValue(stringValue);
+        }}
+        error={meta.touched && meta.error}
+        license={licence}
+        {...props}
+      />
+    </>
+  );
+};
+
+export default FormikTextEditor;
