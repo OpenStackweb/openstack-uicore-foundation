8986c0d1c3c566ac46dbb25e8657405fde968a95
Tomás Castillo <tcastilloboireau@gmail.com>
2026-07-08 15:11:32 -0300

    fix: add new v2 versions of draggable components replacing react-beautiful-dnd for @dnd-kit/sortable
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/components/mui/formik-inputs/additional-input/meta-field-values-v2.js
@@ -0,0 +1,201 @@
+/**
+ * Copyright 2026 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+import React from "react";
+import T from "i18n-react/dist/i18n-react";
+import { useFormikContext } from "formik";
+import { Box, Button, Grid2, Divider, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
+import DragAndDropList from "../../DragNDropList";
+import showConfirmDialog from "../../showConfirmDialog";
+import MuiFormikTextField from "../mui-formik-textfield";
+import MuiFormikCheckbox from "../mui-formik-checkbox";
+
+const MetaFieldValuesV2 = ({
+  field,
+  fieldIndex,
+  baseName = "meta_fields",
+  onMetaFieldTypeValueDeleted,
+  entityId
+}) => {
+  const { values, setFieldValue } = useFormikContext();
+
+  const metaFields = values[baseName] || [];
+  const sortedValues = [...field.values].sort((a, b) => a.order - b.order);
+
+  const buildValueFieldName = (valueIndex, fieldName) =>
+    `${baseName}[${fieldIndex}].values[${valueIndex}].${fieldName}`;
+
+  const onReorder = (newValues) => {
+    const newMetaFields = [...metaFields];
+    newMetaFields[fieldIndex].values = newValues;
+    setFieldValue(baseName, newMetaFields);
+  };
+
+  const handleAddValue = () => {
+    const newFields = metaFields.map((f, i) =>
+      i === fieldIndex
+        ? { ...f, values: [...f.values, { value: "", name: "", is_default: false }] }
+        : f
+    );
+    setFieldValue(baseName, newFields);
+  };
+
+  const handleDefaultChange = (valueIndex, checked) => {
+    const newFields = [...metaFields];
+    if (checked) {
+      newFields[fieldIndex].values.forEach((v) => {
+        v.is_default = false;
+      });
+    }
+    newFields[fieldIndex].values[valueIndex].is_default = checked;
+    setFieldValue(baseName, newFields);
+  };
+
+  const isMetafieldValueIncomplete = () => {
+    if (field.values.length > 0) {
+      return field.values.some((v) => !v.name?.trim() || !v.value?.trim());
+    }
+    return false;
+  };
+
+  const handleRemoveValue = async (metaFieldValue, valueIndex) => {
+    const isConfirmed = await showConfirmDialog({
+      title: T.translate("general.are_you_sure"),
+      text: T.translate("meta_fields.delete_value_warning"),
+      type: "warning",
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: T.translate("general.yes_delete")
+    });
+
+    if (!isConfirmed) return;
+
+    const removeValueFromFields = () => {
+      const newFields = [...metaFields];
+      newFields[fieldIndex].values = newFields[fieldIndex].values.filter(
+        (_, index) => index !== valueIndex
+      );
+      setFieldValue(baseName, newFields);
+    };
+
+    if (field.id && metaFieldValue.id && onMetaFieldTypeValueDeleted) {
+      onMetaFieldTypeValueDeleted(entityId, field.id, metaFieldValue.id).then(
+        () => removeValueFromFields()
+      );
+    } else {
+      removeValueFromFields();
+    }
+  };
+
+  const renderMetaFieldValue = (val, sortedIndex, { isDragging, dragHandleProps } = {}) => {
+    const originalIndex = field.values.findIndex(
+      (v) => (v.id && v.id === val.id) || v === val
+    );
+    const valueIndex = originalIndex !== -1 ? originalIndex : sortedIndex;
+
+    return (
+      <Box key={val.id || `value-${valueIndex}`}>
+        <Grid2
+          container
+          spacing={2}
+          sx={{
+            alignItems: "start",
+            background: isDragging ? "#ebebeb" : "inherit",
+            boxShadow: isDragging
+              ? "0px 5px 15px rgba(0,0,0,0.3)"
+              : "none",
+            transition: "transform 0.2s ease, box-shadow 0.2s ease",
+            transform: isDragging ? "scale(1.02)" : "none",
+            py: 2
+          }}
+        >
+          <Grid2 size={1} sx={{ display: "flex", alignItems: "center" }}>
+            <IconButton
+              {...dragHandleProps}
+              aria-label="drag to reorder"
+              sx={{ cursor: "grab" }}
+            >
+              <DragIndicatorIcon />
+            </IconButton>
+          </Grid2>
+          <Grid2 size={4}>
+            <MuiFormikTextField
+              name={buildValueFieldName(valueIndex, "name")}
+              placeholder={T.translate(
+                "meta_fields.placeholders.name"
+              )}
+              margin="none"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 size={4}>
+            <MuiFormikTextField
+              name={buildValueFieldName(valueIndex, "value")}
+              placeholder={T.translate(
+                "meta_fields.placeholders.value"
+              )}
+              margin="none"
+              fullWidth
+              InputProps={{
+                endAdornment: (
+                  <IconButton
+                    onClick={() => handleRemoveValue(val, valueIndex)}
+                    aria-label="remove value"
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                )
+              }}
+            />
+          </Grid2>
+          <Grid2 size={3}>
+            <MuiFormikCheckbox
+              name={buildValueFieldName(valueIndex, "is_default")}
+              label={T.translate("meta_fields.is_default")}
+              onChange={(e) =>
+                handleDefaultChange(valueIndex, e.target.checked)
+              }
+            />
+          </Grid2>
+        </Grid2>
+        <Divider />
+      </Box>
+    );
+  };
+
+  return (
+    <Box>
+      <DragAndDropList
+        items={sortedValues}
+        onReorder={onReorder}
+        renderItem={renderMetaFieldValue}
+        idKey="id"
+        updateOrderKey="order"
+      />
+      <Grid2 container spacing={2} sx={{ mt: 2 }} offset={4}>
+        <Button
+          startIcon={<AddIcon />}
+          disabled={isMetafieldValueIncomplete()}
+          onClick={handleAddValue}
+          variant="text"
+        >
+          {T.translate("meta_fields.add_value")}
+        </Button>
+      </Grid2>
+    </Box>
+  );
+};
+
+export default MetaFieldValuesV2;
---
+++ src/components/mui/sortable-table/mui-table-sortable-v2.js
@@ -0,0 +1,328 @@
+/**
+ * Copyright 2026 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+import * as React from "react";
+import T from "i18n-react/dist/i18n-react";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import UnfoldMoreIcon from "@mui/icons-material/UnfoldMore";
+import { IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+  arrayMove
+} from "@dnd-kit/sortable";
+import { visuallyHidden } from "@mui/utils";
+
+import styles from "./styles.module.less";
+
+import {
+  DEFAULT_PER_PAGE,
+  FIFTY_PER_PAGE,
+  TWENTY_PER_PAGE
+} from "../../../utils/constants";
+import showConfirmDialog from "../showConfirmDialog";
+import TableCellContent from "../table/table-content";
+import SortableRow from "./sortable-row";
+
+const getRowId = (row, index, idKey) =>
+  row[idKey] !== undefined && row[idKey] !== null
+    ? String(row[idKey])
+    : String(index);
+
+const MuiTableSortableV2 = ({
+  columns = [],
+  data = [],
+  totalRows,
+  perPage,
+  currentPage,
+  onPageChange,
+  onPerPageChange,
+  onSort,
+  options = { sortCol: "", sortDir: 1 },
+  getName = (item) => item.name,
+  onEdit,
+  onDelete,
+  deleteDialogTitle = null,
+  deleteDialogBody = null,
+  onReorder,
+  idKey = "id",
+  updateOrderKey = "order"
+}) => {
+  const handleChangePage = (_, newPage) => {
+    onPageChange(newPage + 1);
+  };
+
+  const handleChangeRowsPerPage = (ev) => {
+    onPerPageChange(ev.target.value);
+  };
+
+  const basePerPageOptions = [
+    DEFAULT_PER_PAGE,
+    TWENTY_PER_PAGE,
+    FIFTY_PER_PAGE
+  ];
+
+  const customPerPageOptions = basePerPageOptions.includes(perPage)
+    ? basePerPageOptions
+    : [...basePerPageOptions, perPage].sort((a, b) => a - b);
+
+  const { sortCol, sortDir } = options;
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
+  );
+
+  const handleDragEnd = ({ active, over }) => {
+    if (!over || active.id === over.id) return;
+
+    const oldIndex = data.findIndex(
+      (row, i) => getRowId(row, i, idKey) === active.id
+    );
+    const newIndex = data.findIndex(
+      (row, i) => getRowId(row, i, idKey) === over.id
+    );
+
+    if (oldIndex === -1 || newIndex === -1) return;
+
+    const movedItem = data[oldIndex];
+    const reordered = arrayMove(data, oldIndex, newIndex);
+
+    // change value based on updateOrderKey
+    if (updateOrderKey) {
+      reordered.forEach((item, idx) => {
+        item[updateOrderKey] = idx + 1;
+      });
+    }
+
+    const movedItemId = movedItem.id;
+    const newOrder = reordered.find(
+      (item) => item[idKey || "id"] === movedItemId
+    )?.[updateOrderKey];
+
+    onReorder?.(reordered, movedItemId, newOrder);
+  };
+
+  const handleDelete = async (item) => {
+    const isConfirmed = await showConfirmDialog({
+      title: deleteDialogTitle || T.translate("general.are_you_sure"),
+      text:
+        typeof deleteDialogBody === "function"
+          ? deleteDialogBody(getName(item))
+          : deleteDialogBody ||
+            `${T.translate("general.row_remove_warning")} ${getName(item)}`,
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: T.translate("general.yes_delete")
+    });
+
+    if (isConfirmed) {
+      onDelete(item.id);
+    }
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper elevation={0} sx={{ width: "100%", mb: 2 }}>
+        <TableContainer
+          component={Paper}
+          sx={{ borderRadius: 0, boxShadow: "none" }}
+        >
+          <Table>
+            {/* TABLE HEADER */}
+            <TableHead sx={{ backgroundColor: "#EAEAEA" }}>
+              <TableRow>
+                {columns.map((col) => (
+                  <TableCell
+                    key={col.columnKey}
+                    sx={{
+                      width: col.width,
+                      minWidth: col.width,
+                      maxWidth: col.width
+                    }}
+                    align={col.align ?? "left"}
+                  >
+                    {col.sortable ? (
+                      <TableSortLabel
+                        active={sortCol === col.columnKey}
+                        direction={
+                          sortCol === col.columnKey && sortDir === -1
+                            ? "desc"
+                            : "asc"
+                        }
+                        onClick={() => onSort(col.columnKey, sortDir * -1)}
+                      >
+                        {col.header}
+                        {sortCol === col.columnKey ? (
+                          <Box component="span" sx={visuallyHidden}>
+                            {sortDir === -1
+                              ? T.translate("mui_table.sorted_desc")
+                              : T.translate("mui_table.sorted_asc")}
+                          </Box>
+                        ) : null}
+                      </TableSortLabel>
+                    ) : (
+                      col.header
+                    )}
+                  </TableCell>
+                ))}
+                {onEdit && <TableCell sx={{ width: 40 }} />}
+                {onDelete && <TableCell sx={{ width: 40 }} />}
+                {onReorder && <TableCell sx={{ width: 40 }} />}
+              </TableRow>
+            </TableHead>
+
+            {/* TABLE BODY */}
+            <DndContext
+              sensors={sensors}
+              collisionDetection={closestCenter}
+              onDragEnd={handleDragEnd}
+            >
+              <SortableContext
+                items={data.map((row, i) => getRowId(row, i, idKey))}
+                strategy={verticalListSortingStrategy}
+              >
+                <TableBody>
+                  {data.map((row, rowIndex) => (
+                    <SortableRow
+                      key={getRowId(row, rowIndex, idKey)}
+                      id={getRowId(row, rowIndex, idKey)}
+                    >
+                      {({ dragHandleProps }) => (
+                        <>
+                          {/* Main content columns */}
+                          {columns.map((col) => (
+                            <TableCell
+                              key={col.columnKey}
+                              align={col.align ?? "left"}
+                              className={`${
+                                col.dottedBorder && styles.dottedBorderLeft
+                              } ${col.className}`}
+                            >
+                              <TableCellContent row={row} col={col} />
+                            </TableCell>
+                          ))}
+                          {/* Edit column */}
+                          {onEdit && (
+                            <TableCell
+                              align="center"
+                              sx={{ width: 40 }}
+                              className={styles.dottedBorderLeft}
+                            >
+                              <IconButton
+                                size="large"
+                                onClick={() => onEdit(row)}
+                                sx={{ padding: 0 }}
+                              >
+                                <EditIcon fontSize="large" />
+                              </IconButton>
+                            </TableCell>
+                          )}
+                          {/* Delete column */}
+                          {onDelete && (
+                            <TableCell
+                              align="center"
+                              sx={{ width: 40 }}
+                              className={styles.dottedBorderLeft}
+                            >
+                              <IconButton
+                                size="large"
+                                onClick={() => handleDelete(row)}
+                                sx={{ padding: 0 }}
+                              >
+                                <DeleteIcon fontSize="large" />
+                              </IconButton>
+                            </TableCell>
+                          )}
+                          {/* Re order column */}
+                          {onReorder && (
+                            <TableCell
+                              align="center"
+                              sx={{ width: 40 }}
+                              className={styles.dottedBorderLeft}
+                              {...dragHandleProps}
+                            >
+                              <IconButton size="large">
+                                <UnfoldMoreIcon fontSize="large" />
+                              </IconButton>
+                            </TableCell>
+                          )}
+                        </>
+                      )}
+                    </SortableRow>
+                  ))}
+                  {data.length === 0 && (
+                    <TableRow>
+                      <TableCell colSpan={columns.length} align="center">
+                        {T.translate("mui_table.no_items")}
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </SortableContext>
+            </DndContext>
+          </Table>
+        </TableContainer>
+
+        {/* PAGINATION */}
+        {onPerPageChange && onPageChange && (
+          <TablePagination
+            component="div"
+            count={totalRows}
+            rowsPerPageOptions={customPerPageOptions}
+            rowsPerPage={perPage}
+            page={currentPage - 1}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            labelRowsPerPage={T.translate("mui_table.rows_per_page")}
+            sx={{
+              ".MuiTablePagination-toolbar": {
+                alignItems: "baseline",
+                marginTop: "1.6rem"
+              },
+              ".MuiTablePagination-spacer": {
+                display: "none"
+              },
+              ".MuiTablePagination-displayedRows": {
+                marginLeft: "auto"
+              }
+            }}
+          />
+        )}
+      </Paper>
+    </Box>
+  );
+};
+
+export default MuiTableSortableV2;
